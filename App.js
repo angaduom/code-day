118805167583befeac7031ecff5ca456ff71730d
Angad Singh <Angad.Singh3@ibm.com>
2018-06-10 17:06:39 -0400

    My fisrt commit for React Native app, says hello

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ToastAndroid, Button, StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  
+	onPressLearnMore() {
+		console.log("Hi Xiaolei!");
+		ToastAndroid.show('A pikachu appeared nearby !', ToastAndroid.SHORT);
+	}
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello Julia!</Text>
+        <Text style={styles.text}>This is an Android app.</Text>
+        <Text style={styles.text}>Made using react native.</Text>
+	<Button
+  		onPress={this.onPressLearnMore}
+ 		title="Hello Hershey"
+  		color="red"
+  		accessibilityLabel="Learn more about this purple button"
+	/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#234567'
+  },
+
+  text: {
+	color:'whitesmoke'
+  }
+});
